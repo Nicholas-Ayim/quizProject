896472dcf463bb296f1ae612710fa29d62512efa
nicholas ayim <99901352+Nicholas-Ayim@users.noreply.github.com>
2023-08-03 00:08:23 +0000

    Add files via upload
    final update

---
+++ questions.js
@@ -0,0 +1,58 @@
+//creating array and passing the number,questions,options and answer
+let questions = [
+    {
+        numb: 1,
+        question: "what is my name?",
+        answer: "nayy",
+        options:[
+            "nayy",
+            "banku",
+            "sakawa",
+            "nicholas"
+        ]
+    },
+    {
+        numb: 2,
+        question: "what the name of my pet?",
+        answer: "nicholas",
+        options:[
+            "nayy",
+            "ayim",
+            "yaw",
+            "nicholas"
+        ]
+    },
+    {
+        numb: 3,
+        question: "what is my food?",
+        answer: "yaw",
+        options:[
+            "nayy",
+            "ayim",
+            "yaw",
+            "nicholas"
+        ]
+    },
+    {
+        numb: 4,
+        question: "what is my dress?",
+        answer: "ayim",
+        options:[
+            "nayy",
+            "ayim",
+            "yaw",
+            "nicholas"
+        ]
+    },
+    {
+        numb: 5,
+        question: "how old are you?",
+        answer: "15",
+        options:[
+            "15",
+            "20",
+            "98",
+            "27"
+        ]
+    },
+]
